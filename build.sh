f0877500570a4bac7c0bddf13e58aa7a7402a1c1
Vincent S. Cojot <vcojot@redhat.com>
2024-05-13 09:17:51 -0400

    add gitlab to hosts

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 IMAGE_NAME=krynn-vbmc
-docker build -t ${IMAGE_NAME} .
+docker build -t ${IMAGE_NAME} . \
+--add-host=gitlab.lasthome.solace.krynn:10.0.129.186 \
+--add-host=ca8.lasthome.solace.krynn:10.0.130.186
